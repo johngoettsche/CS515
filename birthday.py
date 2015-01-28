980384d47d1edcff4bd19c8cc6e1d0773795dbca
John Goettsche <goet0969@vandals.uidaho.edu>
2015-01-27 18:02:35 -0800

    again

--- birthday.py
+++ birthday.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+plist = [-1 for x in range(101)]
 def P(n) :
+	if plist[n] != -1 :
+		return plist[n]
 	if n <= 1 :
+		plist[n] = 1
 		return 1
 	else :
-		return P(n - 1) * ((365 - (n - 1)) / 365.0)
-		
+		plist[n] = P(n - 1) * ((365 - (n - 1)) / 365.0)
+		return P(n - 1) * ((365 - (n - 1)) / 365.0)	
+
 def main() :
 	for x in range(1, 101) :
 		match = P(x)
--- walk.py
+++ walk.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 
+table = [[0 for x in range(13)] for x in range(13)]
+tableWidths = [0 for x in range(-1, 13)]
 def w(N, E) :
-	if N == 0 or E == 0:
-		return 1
+	if table[N][E] != 0 :
+		return table[N][E]
 	else :
-		return w(N - 1, E) + w(N, E - 1)
-
-def main() :
-	table = [[0 for x in range(13)] for x in range(13)]
+		if N == 0 or E == 0:
+			table[N][E] = 1
+		else :
+			table[N][E] = w(N - 1, E) + w(N, E - 1)
+		return table[N][E]
+		
+def setColWidth() :
+	tableWidths[-1] = 2
 	for n in range(0, 13) :
 		for e in range(0, 13) :
-			table[n][e] = w(n, e)
-			print(table[n][e])
+			if len(str(table[n][e])) > tableWidths[e] :
+				tableWidths[e] = len(str(table[n][e]))
+				
+def printTable() :
+	for n in range(-1, 13) :
+		for e in range(-1, 13) :
+			if n < 0 or e < 0 :
+				if n == e :
+					print("x".rjust(tableWidths[e]), end=" ")
+				else :
+					if n < 0 :
+						print(str(e).rjust(tableWidths[e]), end=" ")
+					if e < 0 :
+						print(str(n).rjust(tableWidths[e]), end=" ")
+			else :
+				print(str(table[n][e]).rjust(tableWidths[e]), end=" ")
+		print()
+
+def main() :
+	for n in range(0, 13) :
+		for e in range(0, 13) :
+			w(n, e)
+	setColWidth()
+	printTable()
 			
 main()
 		
